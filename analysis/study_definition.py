22fc8914de05273c975483a22eaaba0e4d1c0318
markdrussell <100200343+markdrussell@users.noreply.github.com>
2022-02-22 18:50:47 +0000

    Ongoing

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,6 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import StudyDefinition, patients, codelists, codelist_from_csv  # NOQA
 
+from codelists import *
 
 study = StudyDefinition(
     default_expectations={
@@ -7,6 +8,11 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
+
+    index_date=(
+        "2019-09-01"
+    ),
+
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
@@ -17,4 +23,26 @@ study = StudyDefinition(
             "int": {"distribution": "population_ages"},
         },
     ),
+    imd=patients.address_as_of(
+        "2020-02-01",
+        returning="index_of_multiple_deprivation",
+        round_to_nearest=100,
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
+        },
+    ),
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        },
+    ),
+    imid = patients.with_these_clinical_events(
+            ra-sle-psoriasis_codes,
+            returning = "binary_flag",
+            find_first_match_in_period = True,
+            between = [index_date, "today"],
+            return_expectations = {"incidence": 0.2}
+    )
 )
